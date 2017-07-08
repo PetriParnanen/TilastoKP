3533444c7187a18de3d61567700924c1cd054d53
PetriParnanen <pjotri@hotmail.com>
2017-07-08 17:07:06 +0300

    should have api fetches and saves, still missing stuff

--- src/app.js
+++ src/app.js
@@ -71,7 +71,7 @@ var events = [
 ];
 
 //all events for one sport
-app.get("/api/sportlist/sport/:sportid", function(req,res) {
+app.get("/api/eventlist/sport/:sportid", function(req,res) {
 	console.log("get all events for sport:"+req.params.sportid);
 	var sportid = req.params.sportid;
 	var l = events.length;
@@ -118,7 +118,7 @@ app.post("/api/eventlist/sport/:sportid/id:id", function(req,res) {
 			newevent.abbr = req.body.abbr;
 			newevent.order = req.body.order;
 			newevent.sum = req.body.sum;
-			events[i].push(newevent);
+			events[i].events.push(newevent);
 			return;
 		}
 	}
@@ -194,7 +194,7 @@ app.get("/api/playerlist/team/:teamid", function(req,res) {
 
 //get one players team data
 app.get("/api/playerlist/team/:teamid/player:playerid", function(req,res) {
-	console.log("get players:"+req.params.teamid+" team data");
+	console.log("get players:"+req.params.teamid+" team data from team:"+req.params.teamid);
 	var tid = req.params.teamid;
 	var pid = req.params.playerid;
 	var l = teamsplayers.length;
@@ -237,25 +237,185 @@ app.post("/api/playerlist/team:teamid/player:playerid", function(req,res) {
 
 	var newplayer = {};
 	newplayer.id = pid;
-	newplayer.fname = req.params.fname;
-	newplayer.lname = req.params.lname;
+	newplayer.fname = req.body.fname;
+	newplayer.lname = req.body.lname;
 	players.push(newplayer);
 
 	//adding player to 
 	var newteamsplayer = {};
 	newteamsplayer.id = pid;
-	newteamsplayer.nick = req.params.nick;
-	newteamsplayer.number = req.params.number;
+	newteamsplayer.nick = req.body.nick;
+	newteamsplayer.number = req.body.number;
 	newteamsplayer.active = "true";
-	newteamsplayer.jdate = req.params.jdate;
+	newteamsplayer.jdate = req.body.jdate;
 
 	var l = teamsplayers.length;
 	for (i = 0; i < l; i++){
 		if (teamsplayers[i].id = tid){
-			teamsplayers[i].push(newteamsplayer));
+			teamsplayers[i].players.push(newteamsplayer));
 			return;
 		}
 	}
 })
+
+//ottelut
+//listaa kaikki kyseisen joukkueet ottelut, pid=playerid, eid=event id
+var matches = [
+	{tid:"1", matchdata:[
+		{id:1, opponent:"Pekan joukkue", date:"2017-02-02",
+		playerdata:[
+			{pid:1,eventdata:[
+				{eid:"1", value:"1"}, {eid:"2", value:"2"}, {eid:"3", value:"1"},
+				{eid:"4", value:"2"}, {eid:"5", value:"1"}, {eid:"6", value:"2"},
+				{eid:"7", value:"1"}, {eid:"8", value:"2"}, {eid:"9", value:"3"},
+				{eid:"10", value:"1"}, {eid:"11", value:"2"}, {eid:"12", value:"1"},
+				{eid:"13", value:"2"}, {eid:"14", value:"1"}, {eid:"15", value:"6"}]
+			},
+			{pid:2,eventdata:[
+				{eid:"1", value:"0"}, {eid:"2", value:"1"}, {eid:"3", value:"0"},
+				{eid:"4", value:"1"}, {eid:"5", value:"0"}, {eid:"6", value:"1"},
+				{eid:"7", value:"0"}, {eid:"8", value:"1"}, {eid:"9", value:"1"},
+				{eid:"10", value:"0"}, {eid:"11", value:"1"}, {eid:"12", value:"0"},
+				{eid:"13", value:"1"}, {eid:"14", value:"0"}, {eid:"15", value:"0"}]
+			},
+			{pid:3,eventdata:[
+				{eid:"1", value:"4"}, {eid:"2", value:"4"}, {eid:"3", value:"4"},
+				{eid:"4", value:"4"}, {eid:"5", value:"4"}, {eid:"6", value:"4"},
+				{eid:"7", value:"4"}, {eid:"8", value:"4"}, {eid:"9", value:"8"},
+				{eid:"10", value:"4"}, {eid:"11", value:"4"}, {eid:"12", value:"4"},
+				{eid:"13", value:"4"}, {eid:"14", value:"4"}, {eid:"15", value:"24"}]
+			},
+			{pid:4,eventdata:[
+				{eid:"1", value:"3"}, {eid:"2", value:"3"}, {eid:"3", value:"3"},
+				{eid:"4", value:"3"}, {eid:"5", value:"3"}, {eid:"6", value:"3"},
+				{eid:"7", value:"3"}, {eid:"8", value:"3"}, {eid:"9", value:"6"},
+				{eid:"10", value:"3"}, {eid:"11", value:"3"}, {eid:"12", value:"3"},
+				{eid:"13", value:"3"}, {eid:"14", value:"3"}, {eid:"15", value:"18"}]
+			},
+			{pid:5,eventdata:[
+				{eid:"1", value:"2"}, {eid:"2", value:"2"}, {eid:"3", value:"2"},
+				{eid:"4", value:"2"}, {eid:"5", value:"2"}, {eid:"6", value:"2"},
+				{eid:"7", value:"2"}, {eid:"8", value:"2"}, {eid:"9", value:"4"},
+				{eid:"10", value:"2"}, {eid:"11", value:"2"}, {eid:"12", value:"2"},
+				{eid:"13", value:"2"}, {eid:"14", value:"2"}, {eid:"15", value:"12"}]
+			},
+			{pid:6,eventdata:[
+				{eid:"1", value:"0"}, {eid:"2", value:"2"}, {eid:"3", value:"0"},
+				{eid:"4", value:"2"}, {eid:"5", value:"0"}, {eid:"6", value:"2"},
+				{eid:"7", value:"0"}, {eid:"8", value:"2"}, {eid:"9", value:"2"},
+				{eid:"10", value:"0"}, {eid:"11", value:"0"}, {eid:"12", value:"0"},
+				{eid:"13", value:"0"}, {eid:"14", value:"0"}, {eid:"15", value:"0"}]
+			}
+		]},
+		{id:2, opponent:"Heikin joukkue", date:"2017-03-03",
+		playerdata:[
+			{pid:3,eventdata:[
+				{eid:"1", value:"1"}, {eid:"2", value:"2"}, {eid:"3", value:"1"},
+				{eid:"4", value:"2"}, {eid:"5", value:"1"}, {eid:"6", value:"2"},
+				{eid:"7", value:"1"}, {eid:"8", value:"2"}, {eid:"9", value:"3"},
+				{eid:"10", value:"1"}, {eid:"11", value:"2"}, {eid:"12", value:"1"},
+				{eid:"13", value:"2"}, {eid:"14", value:"1"}, {eid:"15", value:"6"}]
+			},
+			{pid:4,eventdata:[
+				{eid:"1", value:"0"}, {eid:"2", value:"1"}, {eid:"3", value:"0"},
+				{eid:"4", value:"1"}, {eid:"5", value:"0"}, {eid:"6", value:"1"},
+				{eid:"7", value:"0"}, {eid:"8", value:"1"}, {eid:"9", value:"1"},
+				{eid:"10", value:"0"}, {eid:"11", value:"1"}, {eid:"12", value:"0"},
+				{eid:"13", value:"1"}, {eid:"14", value:"0"}, {eid:"15", value:"0"}]
+			},
+			{pid:5,eventdata:[
+				{eid:"1", value:"4"}, {eid:"2", value:"4"}, {eid:"3", value:"4"},
+				{eid:"4", value:"4"}, {eid:"5", value:"4"}, {eid:"6", value:"4"},
+				{eid:"7", value:"4"}, {eid:"8", value:"4"}, {eid:"9", value:"8"},
+				{eid:"10", value:"4"}, {eid:"11", value:"4"}, {eid:"12", value:"4"},
+				{eid:"13", value:"4"}, {eid:"14", value:"4"}, {eid:"15", value:"24"}]
+			},
+			{pid:6,eventdata:[
+				{eid:"1", value:"3"}, {eid:"2", value:"3"}, {eid:"3", value:"3"},
+				{eid:"4", value:"3"}, {eid:"5", value:"3"}, {eid:"6", value:"3"},
+				{eid:"7", value:"3"}, {eid:"8", value:"3"}, {eid:"9", value:"6"},
+				{eid:"10", value:"3"}, {eid:"11", value:"3"}, {eid:"12", value:"3"},
+				{eid:"13", value:"3"}, {eid:"14", value:"3"}, {eid:"15", value:"18"}]
+			},
+			{pid:7,eventdata:[
+				{eid:"1", value:"2"}, {eid:"2", value:"2"}, {eid:"3", value:"2"},
+				{eid:"4", value:"2"}, {eid:"5", value:"2"}, {eid:"6", value:"2"},
+				{eid:"7", value:"2"}, {eid:"8", value:"2"}, {eid:"9", value:"4"},
+				{eid:"10", value:"2"}, {eid:"11", value:"2"}, {eid:"12", value:"2"},
+				{eid:"13", value:"2"}, {eid:"14", value:"2"}, {eid:"15", value:"12"}]
+			},
+			{pid:8,eventdata:[
+				{eid:"1", value:"0"}, {eid:"2", value:"2"}, {eid:"3", value:"0"},
+				{eid:"4", value:"2"}, {eid:"5", value:"0"}, {eid:"6", value:"2"},
+				{eid:"7", value:"0"}, {eid:"8", value:"2"}, {eid:"9", value:"2"},
+				{eid:"10", value:"0"}, {eid:"11", value:"0"}, {eid:"12", value:"0"},
+				{eid:"13", value:"0"}, {eid:"14", value:"0"}, {eid:"15", value:"0"}]
+			}
+		]}
+	]}
+];
+
+//get all matches for team X
+app.get("/api/matchlist/team/:teamid", function(req,res) {
+	console.log("get all matchess for team:"+req.params.teamid);
+	var tid = req.params.teamid;
+	var l = teamsplayers.length;
+	for (var i = 0; i < l; i++){
+		if (teamid = matches[i].tid) {
+			res.send(JSON.stringify(matches[i]));
+			return;
+		}
+	}
+	var empty = {};
+	res.send(JSON.stringify(empty));
+});
+
+//get match
+app.get("/api/matchlist/team/:teamid/match:matchid", function(req,res) {
+	console.log("get match:"+req.params.matchid+" from team:"+req.params.teamid);
+	var tid = req.params.teamid;
+	var mid = req.params.matchid;
+	var l = matches.length;
+	for (var i = 0; i < l; i++){
+		if (tid = matches[i].tid) {
+			var m = matches[i].matchdata.length;
+			for (var j = 0; j < m; j++){
+				if (matches[i].matchdata[j].id = mid){
+					res.send(JSON.stringify(matches[i].matchdata[j]));
+					return;
+				}
+			}
+		}
+	}
+	var empty = {};
+	res.send(JSON.stringify(empty));
+});
+
+//save match
+app.post("/api/matchlist/team:teamid/match:matchid", function(req,res) {
+	console.log("save match");
+	var tid = req.params.teamid;
+	var mid = req.params.matchid;
+	
+	var newmatch = {};
+	newmatch.id = mid;
+	newmatch.opponent = req.body.opponent;
+	newmatch.date = req.body.date;
+	newmatch.playerdata = req.body.playerdata;
+	var l = matches.length;
+
+	for (var i = 0; i < l; i++){
+		if (tid == matches[i].tid){
+				matches[i].matchdata.push(newmatch);
+				return;
+		}
+	}
+	var newteammatch = {};
+	newteammatch.tid = tid;
+	newteammatch.matchdata = newmatch;
+	matches.push(newteammatch);
+
+});
+
 
 app.listen(3000);
