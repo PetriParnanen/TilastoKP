626282c863dcea013b38b386ca01742ad0aeacdf
Jere Veijalainen <jereveijalainen82@gmail.com>
2017-07-11 11:56:55 +0300

    Haetaan joukkueet apista

--- src/app/scripts/controllers/TeamController.js
+++ src/app/scripts/controllers/TeamController.js
@@ -1,7 +1,9 @@
-angular.module('TeamModule', []).controller('TeamController', function($scope, TeamFactory,$filter){	    
-		var items = TeamFactory.getTeams();
+angular.module('TeamModule', []).controller('TeamController', function($scope,TeamItem,$filter){	    
+		//var items = TeamFactory.getTeams();
+		var items = TeamItem.query();
 	    $scope.teams = items;
 	    console.log($scope.teams);
+
 	   	
 	   	// --- HUOM tämä filtteri ei vielä toimi. Muuttujat keturallaan.
 	    // Näin saadaan valikon ensimmäinen valinta esivalituksi orderBy-filterin mukaisessa järjestyksessä. 
@@ -9,8 +11,11 @@ angular.module('TeamModule', []).controller('TeamController', function($scope, T
 	    //$scope.sortedTeams = $filter('orderBy')($scope.sortedTeams,'name');
 	    // ---
 	    
-	    // Esivalitaan listan ensimmäinen vaihtoehto.
+	    // HUOM Tämä ei nyt toimi kun vaihdettiin käyttöön StatApiFactory
+	    // Esivalitaan listan ensimmäinen vaihtoehto.	    
 	    $scope.selectedTeam = items[0];
+
+	    console.log('items[0]: ' + items[0]);
 
 		// TODO:
 		// Valitun joukkueen id:n säilyminen muihin vieweihin ja sieltä takaisin niin, että valinta säilyy joukkuevalikossa.
