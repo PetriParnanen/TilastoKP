f7deaed6113ace26bc5a165b7757692fd710c617
PetriParnanen <pjotri@hotmail.com>
2018-01-30 00:51:48 +0200

    login backend
    adding login backend

--- src/server.js
+++ src/server.js
@@ -1,11 +1,81 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var session = require("express-session");
+var mongoStore = require("connect-mongo")(session);
+var User = require('./backend/models/user');
 
 var config = require('./config');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//first login and register stuff, keep it in session
+app.use(session({
+    secret:"aBurningHouse",
+    saveUnitialized:false,
+    resave:true,
+    cookie:{maxAge:1000*60*60*24},
+    store:new mongoStore({
+        collection:"session",
+        url:"mongodb://localhost/sessionDb",
+        ttl:24*60*60
+    })
+}));
+
+app.post("/login", function(req,res){
+    console.log("login");
+    User.findOne({ "uname":req.body.uname }, function(err,user){
+        if(err) {
+            res.status(403).send({"Message":"No such user or password"});
+        } else {
+            if (user && req.body.pword == user.pword) { 
+                req.session.logged = true;          
+                res.status(200).json({"uname":user.uname, "token":"user123"});
+            } else {
+                res.status(403).send({"Message":"No such user or password"});
+            }
+        }
+    });
+});
+
+app.post("/logout", function(req, res){
+    console.log("logout");
+    if(req.session){
+        req.session.destroy();
+        res.status(200).send({"Message":"Logout succesfull"});
+    } else {
+        res.status(404).send({"Message":"Failure"});
+    }
+});
+
+app.post("/register", function(req,res){
+    console.log(req.body);
+    var temp = new User({
+        "uname":req.body.uname,
+        "pword":req.body.pword,
+    });
+
+    temp.save(function(err,item){
+        if (err) {
+            console.log(err);
+            res.status(409);
+            res.json({"Message":"Failure"});
+        } else {
+            res.json({"uname":item.uname, "id":item._id});
+        }
+    });
+});
+
+app.use("/api", function(req,res,next){
+    if (req.headers.token == "user123"){
+        req.session.params = req.headers;
+        next();
+    } else {
+        res.status(403).send({"Message":"Not allowed"});
+    }
+});
+
+// handles calls to api path
 var router = express.Router();
 
 router.use(function(req, res, next){
